22571905d73fc32bd118b89826ca995b83713f17
manalechehbi <manale.chehbii@gmail.com>
2023-07-27 11:35:38 +0100

    display folder

--- backend/app/Http/Controllers/FolderController.php
+++ backend/app/Http/Controllers/FolderController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Folder;
 class FolderController extends Controller
 {
-    //
+
+   public function index(){
+    return Folder::select('name','user_id','created_at')->get();
+
+   }
+   public function store(Request $request){
+        $folder = new Folder();
+        $folder->name = $request->folderName;
+        $folder->save();
+
+
+
+      
+
+
+
+ }
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use APP\Http\Controllers\FolderController;
 use App\Http\Controllers\EmployeController;
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,13 @@ use App\Http\Controllers\EmployeController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+/*******************************Folders  ****************************************/
+Route::post('create_folder',[App\Http\Controllers\FolderController::class, 'store']);
+Route::get('folders',[App\Http\Controllers\FolderController::class, 'index']);
 
-
+/*******************************Employes  ****************************************/
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [LoginController::class, 'logout']);
-
-
 Route::resource('Employes',EmployeController::class);
 Route::post('create_employe',[EmployeController::class,'store']);
